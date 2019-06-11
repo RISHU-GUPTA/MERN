25811263bb4320a4650a968bd4c0baa9796f7b20
Rishu Gupta <guptarishu04@gmail.com>
2019-06-11 20:58:48 +0530

    Day 1

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
+//Day 1
 console.log("Hello World !");
+/*Variables can be defined in three ways:
+1. a=10;
+2. var b=12; //function level scope, goes to upward direction; variable hoisting
+3. let c=5; // block level scope, goes to downword , goes to call stack*/
+
+
